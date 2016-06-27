1406ed7ee0f1b4c8947dfde54947b388a60b3ba2
Prateek Jain <prateek.jain23995@yahoo.com>
2016-06-27 23:29:22 +0530

    passing presenter tests

--- app/src/main/java/android/geeknight/com/tdd/controller/LoginController.java
+++ app/src/main/java/android/geeknight/com/tdd/controller/LoginController.java
@@ -6,12 +6,18 @@ import android.geeknight.com.tdd.view.ILoginView;
 public class LoginController {
 
   private ILoginView loginView;
+  private LoginValidator loginValidator;
 
   public LoginController(ILoginView loginView, LoginValidator loginValidator) {
     this.loginView = loginView;
+    this.loginValidator = loginValidator;
   }
 
   public void performLogin(String email, String password) {
-
+    if (loginValidator.isValid(email, password)) {
+      loginView.onLoginSuccess();
+    } else {
+      loginView.onLoginFailure();
+    }
   }
 }
