79a42334c5afcd5a3acc4b62a4fc4df4e32c4eb0
Prateek Jain <prateek.jain23995@yahoo.com>
2016-06-27 23:31:20 +0530

    adding test for model(loginValidator)

---
+++ app/src/test/java/android/geeknight/com/tdd/model/LoginValidatorTest.java
@@ -0,0 +1,25 @@
+package android.geeknight.com.tdd.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoginValidatorTest {
+
+  @Test
+  public void shouldAllowLoginForValidCredentials() throws Exception {
+    LoginValidator loginValidator = new LoginValidator();
+    String email = "gurgaon@thoughtworks.com";
+    String password = "$12345";
+    assertTrue(loginValidator.isValid(email, password));
+  }
+
+  @Test
+  public void shouldDenyLoginForInvalidCredentials() throws Exception {
+    LoginValidator loginValidator = new LoginValidator();
+    String email = "invalidemail@thoughtworks.com";
+    String password = "$12345";
+    assertFalse(loginValidator.isValid(email, password));
+  }
+}
