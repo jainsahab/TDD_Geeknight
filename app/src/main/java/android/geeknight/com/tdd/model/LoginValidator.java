b8125e71b84b83a4129a65425ac531734a580f05
Prateek Jain <prateek.jain23995@yahoo.com>
2016-06-27 23:39:31 +0530

    passing model tests

--- app/src/main/java/android/geeknight/com/tdd/model/LoginValidator.java
+++ app/src/main/java/android/geeknight/com/tdd/model/LoginValidator.java
@@ -2,6 +2,6 @@ package android.geeknight.com.tdd.model;
 
 public class LoginValidator {
   public boolean isValid(String email, String password) {
-    return false;
+    return email.equals("gurgaon@thoughtworks.com") && password.equals("$12345");
   }
 }
